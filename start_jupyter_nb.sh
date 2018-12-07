57ff2dbb10476623b86de81b003b8154cd7d03a4
sfux <samuel.fux@id.ethz.ch>
2018-12-07 13:15:42 +0100

    Update start_jupyter_nb.sh

--- start_jupyter_nb.sh
+++ start_jupyter_nb.sh
@@ -16,7 +16,7 @@ function print_usage {
 }
 
 # if number of command line arguments is different from 4 or if $1==-h or $1==--help
-if [ "$#" -ne 4 ] -o [ "$1" -eq "-h" ] -o [ "$1" -eq "--help" ]; then
+if [ "$#" !=  4 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
         print_usage
         exit
 fi
@@ -33,7 +33,7 @@ if ! [[ "$NUM_CORES" =~ ^[0-9]+$ ]]
         exit
 fi
 # check if NUM_CORES is <= 36
-if [ "37" -gt "$NUM_CORES" ]; then
+if [ "$NUM_CORES" > 36 ]; then
     echo -e "No distributed memory supported, therefore number of cores needs to be smaller or equal to 36"
     print_usage
     exit
