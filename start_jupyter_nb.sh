408ac23b794d54b6c20c974a73caa0c38ffeae84
Jarunan Panyasantisuk <jarunan@ethz.ch>
2021-08-24 15:51:46 +0200

    add config file, remove leonhard open

--- start_jupyter_nb.sh
+++ start_jupyter_nb.sh
@@ -8,6 +8,7 @@
 # 02.04.2020    Added reconnect_info file that contains all information to reconnect to a notebook
 # 17.08.2020    Added a section with configuration options to specify non-standard SSH keys
 # 23.07.2021    Added partial support for windows 10 with git-bash (thank you Henry Lütcke for the input)
+# 24.08.2021    Added config file and removed Leonhard Open
 
 #########################
 # Configuration options #
@@ -32,59 +33,60 @@ fi
 
 # function to print usage instructions
 function print_usage {
-        echo -e "Usage:\t start_jupyter_nb.sh CLUSTER NETHZ_USERNAME NUM_CORES RUN_TIME MEM_PER_CORE\n"
-        echo -e "Arguments:\n"
-        echo -e "CLUSTER\t\t\t Name of the cluster on which the jupyter notebook should be started (Euler or LeoOpen)"
-        echo -e "NETHZ_USERNAME\t\tNETHZ username for which the notebook should be started"
-        echo -e "NUM_CORES\t\tNumber of cores to be used on the cluster (<36)"
-        echo -e "RUN_TIME\t\tRun time limit for the jupyter notebook on the cluster (HH:MM)"
-        echo -e "MEM_PER_CORE\t\tMemory limit in MB per core\n"
-        echo -e "Example:\n"
-        echo -e "./start_jupyter_nb.sh Euler sfux 4 01:20 2048\n"
+        echo -e 'Usage:\t start_jupyter_nb.sh CONFIG_FILE\n'
+        echo -e 'Arguments:\n'
+        echo -e 'CONFIG_FILE format:\t\t'
+        echo -e '    USERNAME=jarunanp'
+        echo -e '    NUM_CORES=4'
+        echo -e '    NUM_GPUS=2'
+        echo -e '    RUN_TIME=4:00'
+        echo -e '    MEM_PER_CORE=2048'
+        echo -e '    MODULES="gcc/6.3.0 python_gpu/3.8.5 eth_proxy"\n'
+        echo -e '    WORKDIR=/cluster/scratch/jarunanp\n'
+        echo -e 'Example:'
+        echo -e '    ./start_jupyter_nb.sh config.txt \n'
 }
 
 # if number of command line arguments is different from 5 or if $1==-h or $1==--help
-if [ "$#" !=  5 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+if [ "$#" !=  1 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
     print_usage
     exit
 fi
+
 
 # find out in which directory the script is located
 SCRIPTDIR=$(pwd)
 
-# Parse and check command line arguments (cluster, NETHZ username, number of cores, run time limit, memory limit per NUM_CORES)
+#========================================================================
+# Parse variables from config file
+#========================================================================
+# Source config file
+source $1
 
-# check on which cluster the script should run and load the proper python module
-CLUSTERNAME="$1"
-
-if [ "$CLUSTERNAME" == "Euler" ]; then
-    CHOSTNAME="euler.ethz.ch"
-    PCOMMAND="new gcc/4.8.2 r/3.6.0 python/3.6.1 eth_proxy"
-elif [ "$CLUSTERNAME" == "LeoOpen" ]; then
-    CHOSTNAME="login.leonhard.ethz.ch"
-    PCOMMAND="r/3.5.1 python_cpu/3.6.4 eth_proxy"
-else
-    echo -e "Incorrect cluster name. Please specify Euler or LeoOpen as cluster and and try again.\n"
-    print_usage
-    exit
-fi
-
+#-----------------
+# CLUSTERNAME
+#-----------------
+CLUSTERNAME="Euler"
+CHOSTNAME="euler.ethz.ch"
 echo -e "\nCluster: $CLUSTERNAME"
 
+#-----------------
+# USERNAME
+#-----------------
 # no need to do checks on the username. If it is wrong, the SSH commands will not work
-USERNAME="$2"
-echo -e "NETHZ username: $USERNAME"
+echo -e "\nUsername: $USERNAME"
+# define the jupyter folder
+JNB_DIR=/cluster/home/$USERNAME/jnb
 
-# number of cores to be used
-NUM_CORES=$3
-
+#-----------------
+# NUM_CORES
+#-----------------
 # check if NUM_CORES is an integer
 if ! [[ "$NUM_CORES" =~ ^[0-9]+$ ]]; then
     echo -e "Incorrect format. Please specify number of cores as an integer and try again.\n"
     print_usage
     exit
 fi
-
 # check if NUM_CORES is <= 36
 if [ "$NUM_CORES" -gt "36" ]; then
     echo -e "No distributed memory supported, therefore number of cores needs to be smaller or equal to 36.\n"
@@ -93,9 +95,27 @@ if [ "$NUM_CORES" -gt "36" ]; then
 fi
 echo -e "Jupyter notebook will run on $NUM_CORES cores"
 
-# run time limit
-RUN_TIME="$4"
 
+#-----------------
+# NUM_GPUS
+#-----------------
+# check if NUM_GPUS is an integer
+if ! [[ "$NUM_GPUS" =~ ^[0-9]+$ ]]; then
+    echo -e "Incorrect format. Please specify number of cores as an integer and try again.\n"
+    print_usage
+    exit
+fi
+# check if NUM_GPUS is <= 8
+if [ "$NUM_GPUS" -gt "8" ]; then
+    echo -e "No distributed memory supported, therefore number of gpus needs to be smaller or equal to 8.\n"
+    print_usage
+    exit
+fi
+echo -e "Jupyter notebook will run on $NUM_GPUS gpus"
+
+#-----------------
+# Run time limit
+#-----------------
 # check if RUN_TIME is provided in HH:MM format
 if ! [[ "$RUN_TIME" =~ ^[0-9][0-9]:[0-9][0-9]$ ]]; then
     echo -e "Incorrect format. Please specify runtime limit in the format HH:MM and try again\n"
@@ -105,9 +125,9 @@ else
     echo -e "Run time limit set to $RUN_TIME"
 fi
 
-# memory per core
-MEM_PER_CORE=$5
-
+#-----------------
+# Memory per core
+#-----------------
 # check if MEM_PER_CORE is an integer
 if ! [[ "$MEM_PER_CORE" =~ ^[0-9]+$ ]]
     then
@@ -117,43 +137,63 @@ if ! [[ "$MEM_PER_CORE" =~ ^[0-9]+$ ]]
 fi
 echo -e "Memory per core set to $MEM_PER_CORE MB\n"
 
+
+#-----------------
+# modules
+#-----------------
+PCOMMAND=$MODULES
+echo -e "Load these modules: $MODULES\n"
+
+#-----------------
 # check if some old files are left from a previous session and delete them
+#-----------------
 echo -e "Checking for left over files from previous sessions"
 if [ -f $SCRIPTDIR/reconnect_info ]; then
         echo -e "Found old reconnect_info file, deleting it ..."
         rm $SCRIPTDIR/reconnect_info
 fi
 ssh $SSH_KEY_OPTION -T $USERNAME@$CHOSTNAME <<ENDSSH
-if [ -f /cluster/home/$USERNAME/jnbinfo ]; then
+if [ -f $JNB_DIR/jnbinfo ]; then
         echo -e "Found old jnbinfo file, deleting it ..."
-        rm /cluster/home/$USERNAME/jnbinfo
+        rm $JNB_DIR/jnbinfo
 fi
-if [ -f /cluster/home/$USERNAME/jnbip ]; then
-	echo -e "Found old jnbip file, deleting it ..."
-        rm /cluster/home/$USERNAME/jnbip
+if [ -f $JNB_DIR/jnbip ]; then
+	      echo -e "Found old jnbip file, deleting it ..."
+        rm $JNB_DIR/jnbip
 fi 
 ENDSSH
 
-# run the jupyter notebook job on Euler/Leonhard Open and save ip, port and the token
-# in the files jnbip and jninfo in the home directory of the user on Euler/Leonhard Open
+#========================================================================
+# Run the jupyter notebook job on Euler
+# and save ip, port and the token in the files jnbip and jninfo
+# in the home directory of the user on Euler
+#========================================================================
 echo -e "Connecting to $CLUSTERNAME to start jupyter notebook in a batch job"
-ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME bsub -n $NUM_CORES -W $RUN_TIME -R "rusage[mem=$MEM_PER_CORE]"  <<ENDBSUB
+ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME bsub -n $NUM_CORES -W $RUN_TIME -R "rusage[mem=$MEM_PER_CORE]" -R "rusage[ngpus_excl_p=$NUM_GPUS]" <<ENDBSUB
+env2lmod
 module load $PCOMMAND
 export XDG_RUNTIME_DIR=
 IP_REMOTE="\$(hostname -i)"
-echo "Remote IP:\$IP_REMOTE" >> /cluster/home/$USERNAME/jnbip
-jupyter notebook --no-browser --ip "\$IP_REMOTE" &> /cluster/home/$USERNAME/jnbinfo 
+mkdir -p $JNB_DIR
+cd $WORKDIR
+echo "Remote IP:\$IP_REMOTE" >> $JNB_DIR/jnbip
+jupyter notebook --no-browser --ip "\$IP_REMOTE" &> $JNB_DIR/jnbinfo 
 ENDBSUB
 
-# wait until jupyternotebook has started, poll every $WAITING_TIME_INTERVAL seconds to check if $HOME/jupyternbinfo exists
-# once the file exists and is not empty, the notebook has been startet and is listening
-ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME "while ! [ -e /cluster/home/$USERNAME/jnbinfo -a -s /cluster/home/$USERNAME/jnbinfo ]; do echo 'Waiting for jupyter notebook to start, sleep for $WAITING_TIME_INTERVAL sec'; sleep $WAITING_TIME_INTERVAL; done"
+#-----------------
+# wait until jupyternotebook has started, poll every $WAITING_TIME_INTERVAL seconds
+# to check if $HOME/jupyternbinfo exists
+# once the file exists and is not empty, the notebook has been started and is listening
+#-----------------
+ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME "while ! [ -e $JNB_DIR/jnbinfo -a -s $JNB_DIR/jnbinfo ]; do echo 'Waiting for jupyter notebook to start, sleep for $WAITING_TIME_INTERVAL sec'; sleep $WAITING_TIME_INTERVAL; done"
 
-# get remote ip, port and token from files stored on Euler/Leonhard Open
+#-----------------
+# get remote ip, port and token from files stored on Euler
+#-----------------
 echo -e "Receiving ip, port and token from jupyter notebook"
-remoteip=$(ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME "cat /cluster/home/$USERNAME/jnbip | grep -m1 'Remote IP' | cut -d ':' -f 2")
-remoteport=$(ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME "cat /cluster/home/$USERNAME/jnbinfo | grep -m1 token | cut -d '/' -f 3 | cut -d ':' -f 2")
-jnbtoken=$(ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME "cat /cluster/home/$USERNAME/jnbinfo | grep -m1 token | cut -d '=' -f 2")
+remoteip=$(ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME "cat $JNB_DIR/jnbip | grep -m1 'Remote IP' | cut -d ':' -f 2")
+remoteport=$(ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME "cat $JNB_DIR/jnbinfo | grep -m1 token | cut -d '/' -f 3 | cut -d ':' -f 2")
+jnbtoken=$(ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME "cat $JNB_DIR/jnbinfo | grep -m1 token | cut -d '=' -f 2")
 
 if  [[ "$remoteip" == "" ]]; then
     echo -e "Error: remote ip is not defined. Terminating script."
@@ -177,12 +217,16 @@ echo -e "Remote IP address: $remoteip"
 echo -e "Remote port: $remoteport"
 echo -e "Jupyter token: $jnbtoken"
 
+#-----------------
 # get a free port on local computer
+#-----------------
 echo -e "Determining free port on local computer"
 PORTN=$(python -c 'import socket; s=socket.socket(); s.bind(("",0)); print(s.getsockname()[1]); s.close()')
 echo -e "Local port: $PORTN"
 
+#-----------------
 # write reconnect_info file
+#-----------------
 echo -e "Restart file \n" >> $SCRIPTDIR/reconnect_info
 echo -e "Remote IP address: $remoteip\n" >> $SCRIPTDIR/reconnect_info
 echo -e "Remote port: $remoteport\n" >> $SCRIPTDIR/reconnect_info
@@ -191,7 +235,9 @@ echo -e "Jupyter token: $jnbtoken\n" >> $SCRIPTDIR/reconnect_info
 echo -e "SSH tunnel: ssh $USERNAME@$CHOSTNAME -L $PORTN:$remoteip:$remoteport -N &\n" >> $SCRIPTDIR/reconnect_info
 echo -e "URL: http://localhost:$PORTN/?token=$jnbtoken\n" >> $SCRIPTDIR/reconnect_info
 
+#-----------------
 # setup SSH tunnel from local computer to compute node via login node
+#-----------------
 echo -e "Setting up SSH tunnel for connecting the browser to the jupyter notebook"
 ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME -L $PORTN:$remoteip:$remoteport -N &
 
@@ -199,7 +245,9 @@ ssh $SSH_KEY_OPTION $USERNAME@$CHOSTNAME -L $PORTN:$remoteip:$remoteport -N &
 # it is established before starting the browser
 sleep 5
 
+#-----------------
 # save url in variable
+#-----------------
 nburl=http://localhost:$PORTN/?token=$jnbtoken
 echo -e "Starting browser and connecting it to jupyter notebook"
 echo -e "Connecting to url "$nburl
